83892a954a8c60741f56120130fee19f4d036676
Alessandro Piras <laynor@gmail.com>
2013-03-21 09:08:31 +0100

    added lua config

---
+++ shared/lua.zsh
@@ -0,0 +1,6 @@
+# /home/ale/local/share/lua/5.2//?.lua;/home/ale/local/share/lua/5.2//?/init.lua;
+export LUA_PATH='/home/ale/local/share/lua/5.2/?.lua;/home/ale/local/share/lua/5.2/?/init.lua;/home/ale/local/lib/lua/5.2/?.lua;/home/ale/local/lib/lua/5.2/?/init.lua;;;'
+export PATH=~/.luarocks/bin:$PATH
+
+
+
